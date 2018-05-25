76b8f7a2c676b500e817056af2849be8de73870e
VsevolodKurochka <dramdoobas123456@gmail.com>
2018-05-25 12:05:50 +0300

    Add particles

--- src/js/vanilla.main.js
+++ src/js/vanilla.main.js
@@ -91,5 +91,10 @@
 		var wow = new WOW();
 		wow.init();
 
+		/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+		particlesJS.load('particles-js', 'js/particles.json', function() {
+		  console.log('callback - particles.js config loaded');
+		});
+
 	});
 }());
